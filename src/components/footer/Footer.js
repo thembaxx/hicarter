eaa891d9eb3964c193498d4c0fa2b243d76bec68
Themba <mndebele.themba@gmail.com>
2022-04-16 17:38:24 +0200

    Add footer section

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -18,20 +18,36 @@ const Footer = () => {
         </div>
       </div>
       <div className={`${styles.links}`}>
-        <a className={`${styles.link}`} href="#">
-          privacy policy
-        </a>
-        <a className={`${styles.link}`} href="#">
-          terms of use
-        </a>
-        <a className={`${styles.link}`} href="#">
-          faqs
-        </a>
-        <a className={`${styles.link}`} href="#">
-          blog
-        </a>
+        <div className={`${styles.linksInner}`}>
+          <a className={`${styles.link}`} href="/">
+            About
+          </a>
+          <a className={`${styles.link}`} href="/">
+            Contact Us
+          </a>
+          <a className={`${styles.link}`} href="/">
+            FAQ
+          </a>
+          <a className={`${styles.link}`} href="/">
+            Blog
+          </a>
+        </div>
+        <div className={`${styles.linksInner}`}>
+          <a className={`${styles.link}`} href="/">
+            Privacy Policy
+          </a>
+          <a className={`${styles.link}`} href="/">
+            Terms of Use
+          </a>
+        </div>
       </div>
-      <div className={`${styles.copyright}`}>Copyright 2022 HeyCarter Pty Ltd | All Rights Reserved | HeyCarter Dealerships (Pty) Ltd is an Authorised Financial Services Provider. FSP: 51498</div>
+      <div className={`${styles.copyright}`}>
+        © 2022 HeyCarter Pty Ltd. All Rights Reserved
+      </div>
+      <div className={`${styles.registration}`}>
+        HeyCarter Dealerships (Pty) Ltd is an Authorised Financial Services
+        Provider. FSP: 51498
+      </div>
     </div>
   );
 };
